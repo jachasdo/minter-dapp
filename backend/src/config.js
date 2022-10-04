1c84be2c9e250f037f2807089096c95624de192c
Jachasdo <Oates928@gmail.com>
2022-10-03 19:44:12 -0700

    lol

--- backend/src/config.js
+++ backend/src/config.js
@@ -61,7 +61,7 @@ const CONTRACT_SYMBOL = 'P';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
 const OWNER_ADDRESS = '0x73e319d250827Db3B9beA17391cBecc5c0553f43';
 const TREASURY_ADDRESS = '0x73e319d250827Db3B9beA17391cBecc5c0553f43';
-const MAX_SUPPLY = 1000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MAX_SUPPLY = 100; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
@@ -69,7 +69,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-01-01T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-02-08T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 720; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x73e319d250827Db3B9beA17391cBecc5c0553f43"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
